ee442b29dff7cd4627cd561bfeef9fc71f9d7322
Bikram Shrestha <stha.bikram999@gmail.com>
2024-02-19 15:19:00 +0545

    Adding Enable Dark Mode and Passing the ToogleFunction as the props

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,12 @@ export default function TextForm(props) {
         let newText=text.toUpperCase();
         setText(newText);
     }
+
     const handleOnChange=(event)=>{
         console.log("Handle OnChange CLicked");
         setText(event.target.value);
     } 
+
     const handleLowClick=()=>{
         let newText=text.toLowerCase();
         setText(newText);
@@ -20,24 +22,32 @@ export default function TextForm(props) {
         let newText="";
         setText(newText);
     }
+    
+    const copyText=()=>{
+        var text=document.getElementById("my-box");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
     const[text, setText]=useState('');
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="my-box"  rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',
+             color:props.mode==='dark'?'white':'black'} } id="my-box"  rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
             <button className='btn btn-primary ms-2' onClick={handleLowClick}>Convert To LowerCase</button>
             <button className="btn btn-primary ms-2" onClick={clearText}>Clear Text</button>
+            <button className="btn btn-primary ms-2" onClick={copyText}>Copy Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1> Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length}</p>
             <p>{0.008*text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter Something to Preview"}</p>
         </div>
         </>
     )

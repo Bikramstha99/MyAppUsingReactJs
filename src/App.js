9748c8ee9af860e58d5e2d5c7a441689512a1a14
Bikram Shrestha <stha.bikram999@gmail.com>
2024-02-27 16:01:23 +0545

    Adding different color mode like success, primary, warning and danger

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
 function App() {
   const [mode, setMode]=useState('light'); //whether dark mode is enabled or not
   const [alert, setAlert]=useState(null);
+  const[color, setColor]=useState('light');
 
   const showAlert=(message,type)=>{
     setAlert({
@@ -27,27 +28,43 @@ function App() {
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
+      removeBodyClasses();
       document.body.style.backgroundColor = 'grey'; 
       showAlert("Dark Mode has been enabled","success")
     } else {
       setMode('light');
+      removeBodyClasses();
       document.body.style.backgroundColor = 'white'; 
       showAlert("Light Mode has been enabled","success")
     }
-  };
+  }
+  const removeBodyClasses=()=>{
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+  }
+
+  const colorMode=(cls)=>{
+    console.log(cls);
+      removeBodyClasses();
+      document.body.classList.add('bg-'+cls);
+      document.body.style.backgroundColor = 'cls'; 
+  }
+
+
   return (
     <> 
     <Router>
-      <Navbar title="Myapp" mode={mode} toggleMode={toggleMode}/> 
+      <Navbar title="Myapp" mode={mode} toggleMode={toggleMode} colorMode={colorMode}/> 
       <Alert alert={alert}  />
       <div className="container" >
       <Routes>
-          <Route exact path="/about" element={<About mode={mode}/>}  />
-          <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode}/> } />
-        </Routes>
+          <Route exact path="/about" element={<About mode={mode} />}  />
+          <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} /> } />
+      </Routes>
         </div>
       </Router>
-      
     </>
   );
 }
